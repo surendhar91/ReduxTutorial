aa237612c8f57cc5a426e9b0d2f4d08f4d0836eb
Sundaresan <susurend@amazon.com>
2017-08-28 18:59:53 +0530

    update the state object, using async call

--- client/index.js
+++ client/index.js
@@ -2,16 +2,25 @@ import {createStore,applyMiddleware} from "redux";
 import logger from "redux-logger";
 import thunk from "redux-thunk";
 import axios from "axios";
+const initialState = {
+    fetching: false,
+    fetched: false,
+    users: [],
+    error: null
+};
 const reducer = (state={},action)=>{
     //working on the action dispatch, create an immutable state object
     switch (action.type){
         case "FETCH_USERS_START":{
+            return {...state, fetching:true};
             break;
         }
         case "FETCH_USERS_ERROR":{
+            return {...state, fetching:false, error:action.command};
             break;
         }
         case "RECEIVE_USERS":{
+            return {...state, fetching:false, fetched:true, users:action.command};
             break;
         }
     }
