bf62c82de4f171f1b209eaa1a7a446b0f470107f
Sundaresan <susurend@amazon.com>
2017-08-28 15:02:17 +0530

    dispatch action-initial

--- client/index.js
+++ client/index.js
@@ -1,8 +1,14 @@
 import {createStore} from "redux";
 
-const reducer = function(){
+const reducer = function(state, action){
 	//working on the action dispatch, create an immutable state object
-	return "foo";
+	if(action.type=='INC'){
+		return state+action.payload;//state is an integer
+	}
+	if(action.type=='DEC'){
+		return state-action.payload;
+	}
+	return state;
 }
 
 const store = createStore(reducer,0);//createStore should always have reducer and the initial state of application
@@ -10,7 +16,12 @@ const store = createStore(reducer,0);//createStore should always have reducer an
 
 //subscribe to changes of action
 store.subscribe(()=>{
+	console.log(store);
 	console.log("store changed", store.getState());
 })
 
 store.dispatch({type:"INC", payload:1});
+store.dispatch({type:"INC", payload:2});
+store.dispatch({type:"INC", payload:3});
+store.dispatch({type:"DEC", payload:100});
+store.dispatch({type:"INC", payload:400});
