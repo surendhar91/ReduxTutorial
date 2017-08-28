53f6574172a03ab77f8af4a2d1c110ac759ce32b
Sundaresan <susurend@amazon.com>
2017-08-28 18:47:55 +0530

    AJAX Asynchronous call to update the store

--- client/index.js
+++ client/index.js
@@ -1,45 +1,39 @@
 import {createStore,applyMiddleware} from "redux";
-
-const reducer = function(initialState=0, action){
+import logger from "redux-logger";
+import thunk from "redux-thunk";
+import axios from "axios";
+const reducer = (state={},action)=>{
     //working on the action dispatch, create an immutable state object
-    if(action.type=='INC'){
-        return initialState+action.payload;//state is an integer
+    switch (action.type){
+        case "FETCH_USERS_START":{
+            break;
+        }
+        case "FETCH_USERS_ERROR":{
+            break;
+        }
+        case "RECEIVE_USERS":{
+            break;
+        }
     }
-    if(action.type=='DEC'){
-        return initialState-action.payload;
-    }
-    if(action.type=='E'){
-    	throw new Error("Error thrown..!!@");
-	}
-    console.log("initial state is",initialState);
-    return initialState;
+    return state;
 }
-const logger = (store)=>(next)=>(action)=>{
-	//middleware - Handler to the action, before calling the reducer. we can intercept the action and change it / cancel it.
-	console.log("action fired", action);
-	// action.type = "DEC";
-	next(action);//triggers the next middleware, if we are not including this, we are simply terminating the redux dispatch action.
-
-}
-const errorHandler = (store)=>(next)=>(action)=>{
-    try{
-        next(action);
-    }catch(e){
-        console.log("Exception !!",e);
-    }
-}
-const store = createStore(reducer,1, applyMiddleware(logger,errorHandler));//createStore should always have reducer and the initial state of application
-
+const middleware = applyMiddleware(thunk, logger);//adding a logger
+const store = createStore(reducer,middleware);//createStore should always have reducer and the initial state of application
 
 
 //subscribe to changes of action
 store.subscribe(()=>{
-	 console.log("store changed", store.getState());
+    console.log("store changed", store.getState());
 });
-
-store.dispatch({type:"INC", payload:1});
-store.dispatch({type:"INC", payload:2});
-store.dispatch({type:"INC", payload:3});
-store.dispatch({type:"DEC", payload:100});
-store.dispatch({type:"INC", payload:400});
-store.dispatch({type:"E"});
+//actions must be plain objects, if we would require an async call, then use a custom middleware: eg., thunk
+store.dispatch((dispatch)=>{
+  dispatch({type:"FETCH_USERS_START"});
+  //Make an asynchronous call
+    axios.get("http://rest.learncode.academy/api/wstern/users").then(
+        (response)=>{
+            dispatch({type:"RECEIVE_USERS", command: response.data});
+        }
+    ).catch((err)=>{
+        dispatch({type:"FETCH_USERS_ERROR", command:err});
+    })
+});
